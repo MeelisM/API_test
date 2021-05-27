3e320321d05869b0895623acd253f1cc3e7ab855
Kaido24 <mumm.meelis@gmail.com>
2021-05-27 18:47:42 +0300

    fixed database connection

---
+++ src/models/facts.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  fact: {
+    type: String,
+  },
+});
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('facts', schema);
--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { getFacts, addFacts } = require('./controllers');
 
-router.get('/chuckFacts', getFacts );
-router.post('/chuckFacts/add', addFacts );
+router.get('/chuckFacts', getFacts);
+router.post('/chuckFacts/add', addFacts);
 
 module.exports = router;
