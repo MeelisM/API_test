488cea73e2a8709b5054c6c0f552fa1ee5deea6d
Kaido24 <73930233+MeelisM@users.noreply.github.com>
2021-05-27 23:50:19 +0300

    small edits

--- src/controllers/addFacts.controller.js
+++ src/controllers/addFacts.controller.js
@@ -4,7 +4,7 @@ const Facts = db.Facts;
 module.exports = async function (req, res) {
   try {
     await Facts.create(req.body);
-    res.status(200).json({ message: 'Success!' });
+    res.status(200).json({ message: 'Added!' });
   } catch (error) {
     res.status(500).json({ message: error });
   }
--- src/controllers/getFacts.controller.js
+++ src/controllers/getFacts.controller.js
@@ -4,7 +4,7 @@ const Facts = db.Facts;
 module.exports = async function (req, res) {
   try {
     const getFact = await Facts.aggregate([{ $sample: { size: 1 } }]);
-    res.status(200).json({ getFact });
+    res.status(200).json(getFact);
   } catch (error) {
     res.status(500).json({ message: error });
   }
