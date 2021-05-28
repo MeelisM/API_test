4a1afd5f65f80c6cb15471abda587e5fc34932e9
Kaido24 <73930233+MeelisM@users.noreply.github.com>
2021-05-28 10:02:35 +0300

    changed getFacts return datatype

--- src/controllers/getFacts.controller.js
+++ src/controllers/getFacts.controller.js
@@ -4,7 +4,8 @@ const Facts = db.Facts;
 module.exports = async function (req, res) {
   try {
     const getFact = await Facts.aggregate([{ $sample: { size: 1 } }]);
-    res.status(200).json(getFact);
+    const chuckFact = getFact[0].fact
+    res.status(200).json(chuckFact);
   } catch (error) {
     res.status(500).json({ message: error });
   }
