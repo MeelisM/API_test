d4006aae5ae2259c96332785a69cb439209811d9
Kaido24 <mumm.meelis@gmail.com>
2021-06-16 07:50:42 +0300

    Added JWT token auth

--- src/router.js
+++ src/router.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const { getFacts, addFacts } = require('./controllers');
+const verifyToken = require('../src/middleware');
 
-router.get('/chuckFacts', getFacts);
+router.get('/chuckFacts', verifyToken, getFacts);
 router.post('/chuckFacts/add', addFacts);
+
+router.post('/login', (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = jwt.sign(user, process.env.JWT_TOKEN);
+  res.json({ accessToken: accessToken });
+});
 
 module.exports = router;
